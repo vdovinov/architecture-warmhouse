a54d7701f498406e1b1f9fdf3f691027b2efa052
Andrey Vdovinov <andrey.vdovinov@gmail.com>
2025-08-30 21:40:13 +0300

    change Componet diagram, Container diagram, docker-compose.yml, DockerFile, app.py (API)

--- temperature_api/app.py
+++ temperature_api/app.py
@@ -1,34 +1,52 @@
 from flask import Flask, request, jsonify
 import random
 
+from datetime import datetime, timezone
+
 app = Flask(__name__)
+
+SENSOR_LOCATIONS = {
+    "1": "Living Room",
+    "2": "Bedroom",
+    "3": "Kitchen"
+}
 
 @app.route('/temperature')
 def temperature():
     location = request.args.get('location', '')
     sensorID = request.args.get('sensorID', '')
     if not location:
-        if sensorID == "1":
+        if sensorID == 1:
             location = "Living Room"
-        elif sensorID == "2":
+        elif sensorID == 2:
             location = "Bedroom"
-        elif sensorID == "3":
+        elif sensorID == 3:
             location = "Kitchen"
         else:
             location = "Unknown"
 
     if not sensorID:
         if location == "Living Room":
-            sensorID = "1"
+            sensorID = 1
         elif location == "Bedroom":
-            sensorID = "2"
+            sensorID = 2
         elif location == "Kitchen":
-            sensorID = "3"
+            sensorID = 3
         else:
-            sensorID = "0"
+            sensorID = 0
 
     temp = round(random.uniform(-20, 40), 2)
-    return jsonify({'location': location, 'sensor': sensorID, 'temperature': temp})
+    return jsonify({'value': temp, 'unit': '°C', 'location': location, 'status': 'active', 'timestamp': str(datetime.now().astimezone().isoformat(timespec='seconds')), 'description': ''})
+
+@app.route('/temperature/<sensor_id>')
+def temperature_by_id(sensor_id):
+    location = SENSOR_LOCATIONS.get(sensor_id, "Unknown")
+
+    if location == "Unknown":
+        return jsonify({'error': f"Sensor with id not found."})
+
+    temp = round(random.uniform(-20, 40), 2)
+    return jsonify({'value': temp, 'unit': '°C', 'location': location, 'status': 'active', 'timestamp': str(datetime.now().astimezone().isoformat(timespec='seconds')),'description': '' })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081)
